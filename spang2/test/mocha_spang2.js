f34f879192c174b2bd0f800806c374f3a345ddfc
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2020-01-29 16:28:24 +0900

    use shell scripts in test/ as test cases

--- spang2/test/mocha_spang2.js
+++ spang2/test/mocha_spang2.js
@@ -1,14 +1,32 @@
-var embed_parameter = require('../src/embed_parameter.js');
-var chai = require('chai');
+const embed_parameter = require('../src/embed_parameter.js');
+const chai = require('chai');
+const path = require('path');
 chai.use(require('chai-fs'));
-var assert = chai.assert;
-var fs = require('fs');
-var process = require('process');
+const execSync = require('child_process').execSync;
+const assert = chai.assert;
+const fs = require('fs');
+const process = require('process');
 
 describe('embed_parameter.js', () => {
   it('embedParameter', () => {
-    var sparql = 'SELECT * WHERE { ?s $p ?o }';
-    var parameterMap = { p: 'rdf:type' };
+    const sparql = 'SELECT * WHERE { ?s $p ?o }';
+    const parameterMap = { p: 'rdf:type' };
     assert.equal(embed_parameter.embedParameter(sparql, parameterMap), 'SELECT * WHERE { ?s rdf:type ?o }');
+  });
+});
+
+describe('shell scripts', () => {
+  results = fs.readdirSync('./result');
+  resultNames = results.map(r => path.basename(r, path.extname(r)));
+  tests = fs.readdirSync('./test');
+  tests.forEach(test => {
+    const basename = path.basename(test, '.sh');
+    if(test.endsWith('.sh') && resultNames.includes(basename)) {
+      it(test, () => {
+        const actual = execSync(`./test/${test}`).toString();
+        const expected = fs.readFileSync(`./result/${basename}.txt`).toString();
+        assert.equal(actual, expected);
+      });
+    }
   });
 });
