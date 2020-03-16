8f6e21b3560b4094f942a8c1d4c28ab3b92020fd
Hirokazu Chiba <hchiba1@gmail.com>
2020-03-16 11:40:39 +0900

    add test for VALUES

--- spang2/test/test-05-0-param.sh
+++ spang2/test/test-05-0-param.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-spang2 -e test --param taxid=2 -f json test/test-05.rq
+spang2 -e test --param taxid=2 -f json test/test-05-0.rq
--- spang2/test/test-05-3-param.sh
+++ spang2/test/test-05-3-param.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-spang2 -e test test/test-05.rq --param taxid=2759
+spang2 -e test test/test-05-0.rq --param taxid=2759
