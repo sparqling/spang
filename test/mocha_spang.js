3ae92248b6db8569fdd2d5b8fc1a011e00679202
Hirokazu Chiba <hchiba1@gmail.com>
2020-03-25 06:04:05 +0900

    simplify test/mocha_spang.js

---
+++ test/mocha_spang.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const execSync = require('child_process').execSync;
+const chai = require('chai');
+const assert = chai.assert;
+chai.use(require('chai-fs'));
+
+describe('test', () => {
+  fs.readdirSync('./test').forEach(file => {
+    if (file.endsWith('.sh')) {
+      const basename = path.basename(file, '.sh');
+      it(file, () => {
+        const result = execSync(`./test/${basename}.sh`).toString();
+        const expect = fs.readFileSync(`./test/${basename}.txt`).toString();
+        assert.equal(result, expect);
+      });
+    }
+  });
+});
