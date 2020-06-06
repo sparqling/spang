b41c53ce29f044bb9c744eda2021b0ebcc857b88
Hirokazu Chiba <hchiba1@gmail.com>
2020-06-06 10:16:02 +0900

    refactor mocha_spang.js

--- test/mocha_spang.js
+++ test/mocha_spang.js
@@ -6,14 +6,18 @@ const assert = chai.assert;
 chai.use(require('chai-fs'));
 
 describe('test', () => {
-  fs.readdirSync('./test').forEach(file => {
-    if (file.endsWith('.sh')) {
-      const basename = path.basename(file, '.sh');
-      it(file, () => {
-        const result = execSync(`./test/${basename}.sh`).toString();
-        const expect = fs.readFileSync(`./test/${basename}.txt`).toString();
-        assert.equal(result, expect);
-      });
-    }
+  fs.readdirSync('test').forEach(file => {
+    testFile(file);
   });
 });
+
+function testFile(file) {
+  if (file.endsWith('.sh')) {
+    const basename = path.basename(file, '.sh');
+    it(file, () => {
+      const result = execSync(`./test/${basename}.sh`).toString();
+      const expect = fs.readFileSync(`./test/${basename}.txt`).toString();
+      assert.equal(result, expect);
+    });
+  }
+}
