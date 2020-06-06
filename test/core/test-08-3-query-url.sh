c800429b47d7485bc1f496d4ed5cc7e3b4344995
Hirokazu Chiba <hchiba1@gmail.com>
2020-06-06 20:00:50 +0900

    fix test-08-3-query-url.sh

--- test/core/test-08-3-query-url.sh
+++ test/core/test-08-3-query-url.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-spang2 https://raw.githubusercontent.com/hchiba1/spang2/master/test/test-08-disease_gene.rq
+spang2 https://raw.githubusercontent.com/hchiba1/spang2/master/test/core/test-08-disease_gene.rq
