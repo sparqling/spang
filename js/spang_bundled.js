1c35ae36210480c84cfc4cb20d1a0b2aee62ee2a
Hirokazu Chiba <hchiba1@gmail.com>
2020-08-10 18:59:32 +0900

    update js/spang_bundled.js

--- js/spang_bundled.js
+++ js/spang_bundled.js
@@ -456,31 +456,31 @@ exports.expandPrefixedUri = (arg) => {
 },{"./template_parser":9,"expand-home-dir":163,"fs":116,"sync-request":361}],7:[function(require,module,exports){
 request = require('request')
 const version = require("../package.json").version;
+const acceptHeaderMap = {
+  "xml"      : "application/sparql-results+xml",
+  "json"     : "application/sparql-results+json",
+  "tsv"      : "application/sparql-results+json", // receive as json and format to tsv afterward
+  "text/tsv" : "text/tab-separated-values",
+  "n-triples": "text/plain",
+  "nt"       : "text/plain",
+  "n3"       : "text/rdf+n3",
+  "html"     : "text/html",
+  "bool"     : "text/boolean",
+  "turtle"   : "application/x-turtle",
+  "ttl"      : "application/x-turtle",
+  "rdf/xml"  : "application/rdf+xml",
+  "rdfxml"   : "application/rdf+xml",
+  "rdfjson"  : "application/rdf+json",
+  "rdfbin"   : "application/x-binary-rdf",
+  "rdfbint"  : "application/x-binary-rdf-results-table",
+  "js"       : "application/javascript",
+};
 
 module.exports = (endpoint, query, format, byGet, callback) => {
-  const acceptHeaderMap = {
-    "xml"      : "application/sparql-results+xml",
-    "json"     : "application/sparql-results+json",
-    "tsv"      : "application/sparql-results+json", // receive as json and format to tsv afterward
-    "text/tsv" : "text/tab-separated-values",
-    "n-triples": "text/plain",
-    "nt"       : "text/plain",
-    "n3"       : "text/rdf+n3",
-    "html"     : "text/html",
-    "bool"     : "text/boolean",
-    "turtle"   : "application/x-turtle",
-    "ttl"      : "application/x-turtle",
-    "rdf/xml"  : "application/rdf+xml",
-    "rdfxml"   : "application/rdf+xml",
-    "rdfjson"  : "application/rdf+json",
-    "rdfbin"   : "application/x-binary-rdf",
-    "rdfbint"  : "application/x-binary-rdf-results-table",
-    "js"       : "application/javascript",
-  };
   const accept = acceptHeaderMap[format];
   var options = {
     uri: endpoint, 
-    // form: {query: query},
+    form: {query: query},
     qs: {query: query},
     followAllRedirects: true,
     headers:{ 
@@ -488,7 +488,6 @@ module.exports = (endpoint, query, format, byGet, callback) => {
       "Accept": accept
     }
   };
-  console.log(options);
   if(byGet) {
     request.get(options, callback);
   } else {
