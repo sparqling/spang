5e23dafd6d7dccb1ae92a725ef461216c7f39eae
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2020-09-08 15:47:37 +0900

    Write csv in streaming manner

--- bin/benchmark.js
+++ bin/benchmark.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const { spawnSync } = require("child_process");
-const { convertArrayToCSV } = require('convert-array-to-csv');
+const csvWriter = require('csv-write-stream')
 const ls = require('ls');
 
 
@@ -11,6 +11,7 @@ let jsonPath;
 const commander = require('commander')
       .option('-i, --iteration <ITERATION_NUM>', 'number of iteration of measurement', 1)
       .option('-d, --delimiter <DELIMITER>', 'delimiter of output', ',')
+      .option('-v, --verbose', 'output progress to stderr')
       .arguments('[json_path]')
       .action((s) => {
         jsonPath = s;
@@ -18,6 +19,18 @@ const commander = require('commander')
 
 commander.parse(process.argv);
 
+header = ['name'];
+header = header.concat(Array.from({length:commander.iteration},(_,k)=>k+1));
+header.push('average');
+
+let writer = csvWriter({
+  separator: commander.delimiter,
+  newline: '\n',
+  headers: header,
+  sendHeaders: true
+});
+
+writer.pipe(process.stdout);
 readFile = (path) => fs.readFileSync(path, "utf8").toString();
 
 json = JSON.parse(readFile(commander.args[0]));
@@ -25,30 +38,30 @@ json = JSON.parse(readFile(commander.args[0]));
 let rows = [];
 
 function measureQuery(queryPath, expected){
-  console.error(`${queryPath} started...`);
-  let row = [queryPath];
+  let row = { 'name': queryPath };
   let times = [];
+  if(commander.verbose) console.error(queryPath);
   for(let i = 0; i < commander.iteration; i++) {
-    console.error(`${i}...`);
+    let column = (i+1).toString();
+    if(commander.verbose) console.error(`query: ${column}`);
     let result = spawnSync('spang2', ['--time', queryPath]);
     if(result.status) // error
     {
-      console.error("error!");
-      row.push(result.stderr.toString());
+      row[column] = result.stderr.toString();
     } else {
       let time = result.stderr.toString().match(/(\d+)ms/)[1];
       times.push(time);
       if(!expected || expected === result.stdout.toString()) {
-        console.error(time);
-        row.push(time);
+        row[column] = time;
       } else {
-        row.push(`${time}_wrong`);
+        row[column] = `${time}_wrong`;
       }
     }
+    if(commander.verbose) console.error(row[column]);
   }
   const average = times.map((t) => parseInt(t)).reduce((a, b) => a+b, 0) / times.length;
-  row.push(average.toString());
-  rows.push(row);
+  row['average'] = average.toString();
+  writer.write(row);
 };
 
 for(let benchmark of json)
@@ -59,10 +72,4 @@ for(let benchmark of json)
   }
 }
 
-header = ['name']
-header = header.concat(Array.from({length:commander.iteration},(_,k)=>k+1));
-header.push('average');
-
-csv = convertArrayToCSV(rows, { header, separator: commander.delimiter });
-
-console.log(csv);
+writer.end();
