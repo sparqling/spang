54c083fe7df32794e90ceecdd57f0f307bdcb4e9
Hirokazu Chiba <hchiba1@gmail.com>
2020-12-24 12:54:12 +0900

    fix priority of loading prefixes

--- bin/spang.js
+++ bin/spang.js
@@ -83,6 +83,16 @@ if (commander.args.length < 1) {
   }
 }
 
+if (commander.prefix) {
+  prefixModule.setPrefixFiles(commander.prefix.split(',').map((path) => path.trim()));
+} else if (commander.ignore) {
+  // --prefix has priority over --ignore
+  prefixModule.setPrefixFiles([`${__dirname}/../etc/prefix`]);
+} else {
+  // default paths
+  prefixModule.setPrefixFiles([`${__dirname}/../etc/prefix`, `${require('os').homedir()}/.spang/prefix`]);
+}
+
 if (commander.subject || commander.predicate || commander.object || (commander.limit && !templatePath) || commander.number || commander.graph || commander.from) {
   sparqlTemplate = shortcut(
     { S: commander.subject, P: commander.predicate, O: commander.object, L: commander.limit, N: commander.number, G: commander.graph, F: commander.from },
@@ -103,16 +113,6 @@ if (commander.subject || commander.predicate || commander.object || (commander.l
     commander.parse(args);
   }
   templateSpecified = true;
-}
-
-if (commander.prefix) {
-  prefixModule.setPrefixFiles(commander.prefix.split(',').map((path) => path.trim()));
-} else if (commander.ignore) {
-  // --prefix has priority over --ignore
-  prefixModule.setPrefixFiles([`${__dirname}/../etc/prefix`]);
-} else {
-  // default paths
-  prefixModule.setPrefixFiles([`${__dirname}/../etc/prefix`, `${require('os').homedir()}/.spang/prefix`]);
 }
 
 if (commander.list_nick_name) {
