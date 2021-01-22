bb693a6f07a9434e66b9045973c2672b0e9ae2b9
Hirokazu Chiba <hchiba1@gmail.com>
2021-01-22 09:46:56 +0900

    refactor bin/cypher.js and remove lib/cypher_shortcut.js

--- bin/cypher.js
+++ bin/cypher.js
@@ -4,7 +4,6 @@ fs = require('fs');
 
 const search_db_name = require('../lib/search_db_name');
 const prefixModule = require('../lib/prefix.js');
-const shortcut = require('../lib/cypher_shortcut.js').shortcut;
 const request = require('request');
 const syncRequest = require('sync-request');
 const columnify = require('columnify');
@@ -277,6 +276,69 @@ function constructCypher(queryTemplate, metadata, parameterMap, positionalArgume
   let json = '{\n';
   json += '  "statements": [\n';
   json += `    { "statement": "${cypher.trim()}" }\n`;
+  json += '  ]\n';
+  json += '}\n';
+  return json;
+}
+
+function shortcut(options, prefixMap) {
+  let cypher = 'MATCH ';
+
+  let node = 'n';
+  if (options.n) {
+    node += ` ${options.n}`;
+  }
+  if (options.p) {
+    node += ` { ${options.p} }`;
+  }
+
+  let where = '';
+  if (options.i) {
+    where = ` WHERE id(n)=${options.i}`;
+  }
+
+  let limit = ' LIMIT 10';
+  if (options.L) {
+    limit = ` LIMIT ${options.L}`;
+  }
+
+  let retNode = 'n';
+  if (options.R) {
+    retNode = options.R.split(',').map(v => {
+      return v.includes('.') ? v : `n.${v}`;
+    }).join(',');
+  }
+  retNode += limit;
+  if (options.C) {
+    retNode = 'COUNT(n)';
+  }
+
+  if (options.r) {
+    cypher += `(n1)-[r:${options.r}]->(n2)`;
+    cypher += ' RETURN ';
+    if (options.C) {
+      cypher += 'COUNT(r)';
+    } else {
+      if (options.R) {
+        const vars1 = options.R.split(',').map(v => `n1.${v}`).join(',');
+        const vars2 = options.R.split(',').map(v => `n2.${v}`).join(',');
+        cypher += `${vars1},${vars2}`;
+      } else {
+        cypher += `n1,n2`;
+      }
+      cypher += limit;
+    }
+  // } else if (options.G) {
+  //   cypher += '(n)-[r]->() RETURN COUNT(r)';
+  } else {
+    cypher += `(${node})${where} RETURN ${retNode}`;
+  }
+
+  cypher = cypher.replace(/\"/g, '\\"').trim();
+
+  let json = '{\n';
+  json += '  "statements": [\n';
+  json += `    { "statement": "${cypher}" }\n`;
   json += '  ]\n';
   json += '}\n';
   return json;
