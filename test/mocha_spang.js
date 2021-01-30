6790619900ebdd995ad4571788fdfdea3e3b9aa2
Hirokazu Chiba <hchiba1@gmail.com>
2021-01-30 14:06:05 +0900

    refactor test/mocha_spang.js

--- test/mocha_spang.js
+++ test/mocha_spang.js
@@ -6,19 +6,20 @@ const assert = chai.assert;
 chai.use(require('chai-fs'));
 
 // fs.readdirSync('test').forEach((subdir) => {
+  // if (!fs.statSync(`test/${subdir}`).isDirectory()) {
+  //   return;
+  // }
 ['core', 'fmt', 'iri', 'function', 'beta'].forEach((subdir) => {
-  if (fs.statSync(`test/${subdir}`).isDirectory()) {
-    describe(subdir, () => {
-      fs.readdirSync(`test/${subdir}`).forEach((file) => {
-        if (file.endsWith('.sh')) {
-          const basename = path.basename(file, '.sh');
-          it(file, () => {
-            const result = execSync(`cd test/${subdir}; ./${basename}.sh`).toString();
-            const expect = fs.readFileSync(`test/${subdir}/${basename}.txt`).toString();
-            assert.equal(result, expect);
-          });
-        }
-      });
+  describe(subdir, () => {
+    fs.readdirSync(`test/${subdir}`).forEach((file) => {
+      if (file.endsWith('.sh')) {
+        const basename = path.basename(file, '.sh');
+        it(file, () => {
+          const result = execSync(`cd test/${subdir}; ./${basename}.sh`).toString();
+          const expect = fs.readFileSync(`test/${subdir}/${basename}.txt`).toString();
+          assert.equal(result, expect);
+        });
+      }
     });
-  }
+  });
 });
