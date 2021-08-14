d5c637b306a1a28cb22c9cd92c647f0065afc8c7
Hirokazu Chiba <hchiba1@gmail.com>
2021-08-14 14:33:26 +0900

    Update js/spfmt_browser.js

--- js/spfmt_browser.js
+++ js/spfmt_browser.js
@@ -1,1 +1,5 @@
-spfmt = require('../lib/spfmt.js');
+spfmt = (sparql, indentDepth = 2) => {
+  const parser = require('../lib/template_parser');
+  const formatter = require('../lib/formatter.js');
+  return formatter.format(parser.parse(sparql), indentDepth);
+};
