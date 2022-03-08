d8e7c957016fc26be3903ee69c6f11ab8666b8b2
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2022-03-08 11:32:51 +0900

    Use configuration loaded from config.json

--- web/js/common.js
+++ web/js/common.js
@@ -2,3 +2,15 @@ const repo = "sparqling/spang";
 const tag = "develop";
 
 let selector = document.querySelector.bind(document);
+let configList = null;
+let onConfigLoad = [];
+
+$(() => {
+  $.get(`https://raw.githubusercontent.com/${repo}/${tag}/library/config.json`, (res) => {
+    configList = JSON5.parse(res);
+    for(let handler of onConfigLoad) {
+      handler();
+    }
+  });
+});
+  
