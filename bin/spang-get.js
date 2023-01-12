02eb060fa0358968c8f145ccb135c48f3733f02b
Hirokazu Chiba <hchiba1@gmail.com>
2023-01-12 15:51:15 +0900

    Rename to --ignore-user-config in bin/spang-get.js

--- bin/spang-get.js
+++ bin/spang-get.js
@@ -7,7 +7,7 @@ const alias = require('../lib/alias.js');
 
 let opts = program
   .option('-r, --prefix <PREFIX_FILES>', 'read prefix declarations (default: SPANG_DIR/etc/prefix,~/.spang/prefix)')
-  .option('-n, --ignore-user-prefix', 'ignore user-specific file (~/.spang/prefix) for test purpose')
+  .option('-n, --ignore-user-config', 'ignore user configuration for test purpose')
   .option('-q, --quit', 'show expanded URI and quit')
   .version(require("../package.json").version)
   .arguments('<URI>')
